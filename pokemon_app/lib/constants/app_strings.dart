1833929dfb7f80162300a4c35e9223925d33ed51
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2021-03-11 16:40:45 +0200

    Update Patch:
    1- Finished UI Design of Pokemon Details Page.

--- pokemon_app/lib/constants/app_strings.dart
+++ pokemon_app/lib/constants/app_strings.dart
@@ -2,4 +2,9 @@ class AppStrings {
   static String noDataError = 'no_data_error';
   static String noInternetError = 'no_internet_error';
   static String loadingStateMessage = 'loading_state_message';
+  static String height = 'height';
+  static String weight = 'weight';
+  static String typesHeading = 'types_heading';
+  static String weaknessHeading = 'weakness_heading';
+  static String nextEvolutionHeading = 'next_evolution_heading';
 }
